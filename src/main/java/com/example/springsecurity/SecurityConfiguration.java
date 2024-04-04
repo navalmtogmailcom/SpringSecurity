8711371a89fd3e5a6abf6f5e8aee5801bdb8e8c1
Naval <navalmto@gmail.com>
2024-04-04 14:28:04 +0530

    Autherization

--- src/main/java/com/example/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurity/SecurityConfiguration.java
@@ -2,6 +2,10 @@ package com.example.springsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,10 +13,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration{
     @Bean
+    //Authentication
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
         UserDetails admin = User.withUsername("Basant")
                 .password(encoder.encode("password1"))
@@ -23,6 +31,22 @@ public class SecurityConfiguration{
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/default")
+                .permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/user")
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .build();
     }
 @Bean
     public PasswordEncoder passwordEncoder(){
--- src/main/java/com/example/springsecurity/SpringSecurityDemo.java
+++ src/main/java/com/example/springsecurity/SpringSecurityDemo.java
@@ -1,12 +1,23 @@
 package com.example.springsecurity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringSecurityDemo {
-    @GetMapping("/")
+    @GetMapping("/default")
     public  String securityValidate(){
         return "<h1>Welcomeeeee</h1>";
+    }
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+    public String userback(){
+        return "I am user";
+    }
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String adminback(){
+        return "I am admin";
     }
 }
